98196e9bb195efe5bda0fc320f8cf3b6d83464d3
lokraszewski <lokraszewski@innovative-technology.com>
2018-12-06 12:25:43 +0000

    Added boost crc for comparison

--- bench/ccitt.cpp
+++ bench/ccitt.cpp
@@ -12,7 +12,9 @@
 #include <set>
 #include <vector>
 
+#include <boost/crc.hpp>
 #include <crc/crc.h>
+
 #define BM_LOWER_BYTE_COUNT 8
 #define BM_UPPER_BYTE_COUNT (8 << 12)
 
@@ -48,6 +50,34 @@ static void BM_cpp_crc_ccitt_fast(benchmark::State &state)
   }
 }
 BENCHMARK(BM_cpp_crc_ccitt_fast)->RangeMultiplier(2)->Range(BM_LOWER_BYTE_COUNT, BM_UPPER_BYTE_COUNT);
+
+static void BM_boost_ccitt_basic(benchmark::State &state)
+{
+  boost::crc_basic<16> ccitt     = boost::crc_basic<16>(0x1021, 0xFFFF, 0, false, false);
+  auto                 test_data = generate_test_data(state.range(0));
+
+  while (state.KeepRunning())
+  {
+    ccitt.reset();
+    ccitt.process_bytes(&test_data[0], test_data.size());
+    benchmark::DoNotOptimize(ccitt.checksum());
+  }
+}
+BENCHMARK(BM_boost_ccitt_basic)->RangeMultiplier(2)->Range(BM_LOWER_BYTE_COUNT, BM_UPPER_BYTE_COUNT);
+
+static void BM_boost_ccitt_optimal(benchmark::State &state)
+{
+  boost::crc_optimal<16, 0x1021, 0xFFFF, 0, false, false> ccitt;
+  auto                                                    test_data = generate_test_data(state.range(0));
+
+  while (state.KeepRunning())
+  {
+    ccitt.reset();
+    ccitt.process_bytes(&test_data[0], test_data.size());
+    benchmark::DoNotOptimize(ccitt.checksum());
+  }
+}
+BENCHMARK(BM_boost_ccitt_optimal)->RangeMultiplier(2)->Range(BM_LOWER_BYTE_COUNT, BM_UPPER_BYTE_COUNT);
 
 //~~~~~~~~~~~~~~~~
 
